333b50ec23bd0bcfb0587c1e0adc09589a37029f
Christopher-Bogan <60634997+Christopher-Xavier@users.noreply.github.com>
2020-09-28 13:21:19 -0400

    Add files via upload

---
+++ bin/server.js
@@ -0,0 +1,5 @@
+const app = require('../app');
+
+const port = 3000;
+
+app.listen('3000', (req, res) => console.log(`listening on port ${3000}`));
---
+++ bin/sql/Untitled.sql
@@ -0,0 +1,1 @@
+ALTER TABLE information_schema.sql_languages ADD sql_language_source information_schema.character_data NULL COLLATE "C";
